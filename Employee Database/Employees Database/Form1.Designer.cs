12b7bbe33fd4c4fbd5df40b49e7984a3e39101c5
Steve Hentschel <slhcoding@gmail.com>
2013-10-22 23:20:12 -0400

    First form using SQL to manipulate a simple employee database.  Woot! Covers opening, reading, indexing, updating and deleting items in the db. Also added a search function at the end.

---
+++ Employee Database/Employees Database/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Employees_Database
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(112, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(124, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "First Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Last Name:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(112, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(124, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Job Title:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(112, 121);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(124, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Department:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(112, 158);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(124, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(195, 226);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 8;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(96, 226);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
+            this.btnPrevious.TabIndex = 9;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(96, 200);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(174, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(59, 266);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(75, 23);
+            this.btnFirst.TabIndex = 11;
+            this.btnFirst.Text = "First Record";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(229, 266);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(75, 23);
+            this.btnLast.TabIndex = 12;
+            this.btnLast.Text = "Last Record";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(328, 60);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(110, 23);
+            this.btnAddNew.TabIndex = 13;
+            this.btnAddNew.Text = "Set New Employee";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(328, 102);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(110, 23);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "Save New Record";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "DB Status";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(328, 141);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(124, 23);
+            this.btnUpdate.TabIndex = 16;
+            this.btnUpdate.Text = "Update Employee Info";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(328, 177);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(137, 23);
+            this.btnDelete.TabIndex = 17;
+            this.btnDelete.Text = "Delete Employee Record";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(371, 226);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "label6";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(374, 269);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 19;
+            this.textBox6.Text = "Last Name";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(374, 299);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 20;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 334);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnAddNew);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Employee Database";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ Employee Database/Employees Database/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Employees_Database
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        System.Data.SqlServerCe.SqlCeConnection con;
+        System.Data.SqlServerCe.SqlCeDataAdapter da;
+        DataSet ds1;
+        int MaxRows = 0;
+        int inc = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con = new System.Data.SqlServerCe.SqlCeConnection();
+            con.ConnectionString = "Data Source=C:\\Documents and Settings\\All\\My Documents\\VS Databases\\Employees.sdf";
+            con.Open();
+
+            ds1 = new DataSet();
+            string sql = "SELECT * From tbl_employees";
+            da = new System.Data.SqlServerCe.SqlCeDataAdapter(sql, con);
+
+            da.Fill(ds1, "Workers");
+            MaxRows = ds1.Tables["Workers"].Rows.Count;
+
+            NavigateRecords();
+
+            con.Close();
+        }
+
+        private void NavigateRecords()
+        {
+            DataRow dRow = ds1.Tables["Workers"].Rows[inc];
+            textBox1.Text = dRow.ItemArray.GetValue(1).ToString();
+            textBox2.Text = dRow.ItemArray.GetValue(2).ToString();
+            textBox3.Text = dRow.ItemArray.GetValue(3).ToString();
+            textBox4.Text = dRow.ItemArray.GetValue(4).ToString();
+            textBox5.Clear();
+            RecCount();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (inc < MaxRows - 1)
+            {
+                inc++;
+                NavigateRecords();
+            }
+            else
+            {
+                textBox5.Text = "This is the last record ...";
+            }
+
+
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (inc > 0)
+            {
+                inc--;
+                NavigateRecords();
+            }
+            else
+            {
+                textBox5.Text = "This is the first record ...";
+            }
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            if (inc != 0)
+            {
+                inc = 0;
+                NavigateRecords();
+            }
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            if (inc != MaxRows-1)
+            {
+                inc = MaxRows-1;
+                NavigateRecords();
+            }
+        }
+
+        private void btnAddNew_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+
+            textBox1.Focus();
+            btnSave.Enabled = true;
+            btnAddNew.Enabled = false;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            DataRow dRow = ds1.Tables["Workers"].NewRow();
+
+            dRow[1] = textBox1.Text;
+            dRow[2] = textBox2.Text;
+            dRow[3] = textBox3.Text;
+            dRow[4] = textBox4.Text;
+            ds1.Tables["Workers"].Rows.Add(dRow);
+
+            UpdateDB();
+            textBox5.Text = "New employee record added.";
+
+            MaxRows = MaxRows + 1;
+            inc = MaxRows - 1;
+
+            btnAddNew.Enabled = true;
+            btnSave.Enabled = false;
+        }
+
+        private void UpdateDB()
+        {
+            System.Data.SqlServerCe.SqlCeCommandBuilder cb;
+            cb = new System.Data.SqlServerCe.SqlCeCommandBuilder(da);
+            cb.DataAdapter.Update(ds1.Tables["Workers"]);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataRow dRow2 = ds1.Tables["Workers"].Rows[inc];
+
+            dRow2[1] = textBox1.Text;
+            dRow2[2] = textBox2.Text;
+            dRow2[3] = textBox3.Text;
+            dRow2[4] = textBox4.Text;
+
+            UpdateDB();
+            textBox5.Text = "Employee data updated.";
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            ds1.Tables["Workers"].Rows[inc].Delete();
+            UpdateDB();
+
+            MaxRows = ds1.Tables["Workers"].Rows.Count;
+            inc--;
+            NavigateRecords();
+            textBox5.Text = "Employee record deleted.";
+        }
+
+        private void RecCount()
+        {
+            label6.Text = "< Record " + (inc+1) + " of " + MaxRows + " >";
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            int result = 0; 
+            string searchFor = textBox6.Text;
+            DataRow[] returnedRows;
+
+            returnedRows = ds1.Tables["Workers"].Select("last_name='" + searchFor + "'");
+            result = returnedRows.Length;
+
+            if (result != 0)
+            {
+                DataRow dr1 = returnedRows[0];
+                textBox1.Text = dr1[1].ToString();
+                textBox2.Text = dr1[2].ToString();
+                textBox3.Text = dr1[3].ToString();
+                textBox4.Text = dr1[4].ToString();
+                textBox5.Text = "Last name match was found.";
+            }
+            else
+            {
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+                textBox5.Text = "Search record not found.";
+            }
+        }
+    }
+}
